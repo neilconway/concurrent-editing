e20e7d183e2486dc34f079c0a3398a96a4c9c857
Emily Andrews <emily@tesba.net>
2012-04-08 18:17:20 -0700

    First implementation of insertion methods.  Prettier code and more robust tests to come.

--- treedoc/tc_treenode.rb
+++ treedoc/tc_treenode.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'minitest/autorun'
 require './treedoc'
 
+
 class TreeNodeTest < MiniTest::Unit::TestCase
+
   def test_basic_triangle
     left_mini = MiniNode.new([], [], nil, "a")
     left_node = TreeNode.new([left_mini])
@@ -16,5 +18,60 @@ class TreeNodeTest < MiniTest::Unit::TestCase
     assert_equal(%w[a b c], root_node.to_a)
     assert_equal(%w[a c], root_node.select{|a| a != "b"})
     assert_equal(true, root_node.all?{|a| %w[a b c].include? a})
+
+  end
+
+  def test_find_basic_triangle
+    left_mini = MiniNode.new([], [], nil, "a")
+    left_node = TreeNode.new([left_mini])
+
+    right_mini = MiniNode.new([], [], nil, "c")
+    right_node = TreeNode.new([right_mini])
+    
+    root_mini = MiniNode.new([left_node], [right_node], nil, "b")
+    root_node = TreeNode.new([root_mini])
+    
+    path = [1] 
+    found_node = root_node.find_tree_node(path) 
+    assert_equal(right_node, found_node)
+  end
+
+  def test_basic_insert_after
+    left_mini = MiniNode.new([], [], nil, "a")
+    left_node = TreeNode.new([left_mini])
+
+    right_mini = MiniNode.new([], [], nil, "c")
+    right_node = TreeNode.new([right_mini])
+    
+    root_mini = MiniNode.new([left_node], [right_node], nil, "b")
+    root_node = TreeNode.new([root_mini])
+    
+    path = [1]
+    referenceNode = root_node.find_tree_node(path)
+    assert_equal(referenceNode, right_node)
+    assert_equal(true, referenceNode.check_empty_right())
+
+    root_node.insert_after(path, "d")
+
+    assert_equal(["d"], right_node.minis[0].right[0].to_a)
+  end
+
+  def test_basic_insert_before
+    left_mini = MiniNode.new([], [], nil, "a")
+    left_node = TreeNode.new([left_mini])
+
+    right_mini = MiniNode.new([], [], nil, "c")
+    right_node = TreeNode.new([right_mini])
+    
+    root_mini = MiniNode.new([left_node], [right_node], nil, "b")
+    root_node = TreeNode.new([root_mini])
+    
+    path = [0]
+    referenceNode = root_node.find_tree_node(path)
+    assert_equal(referenceNode, left_node)
+    assert_equal(true, referenceNode.check_empty_left())
+    
+    root_node.insert_before(path, "d")
+    assert_equal(["d"], left_node.minis[0].left[0].to_a)
   end
 end
--- treedoc/treedoc.rb
+++ treedoc/treedoc.rb
@@ -3,19 +3,19 @@ require "bud"
 
 class MiniNode
   include Enumerable
-
+  
   attr_reader :left
   attr_reader :right
   attr_reader :dis
   attr_reader :atom
-
+  
   def initialize(l, r, d, a)
     @left = l
     @right = r
     @dis = d
     @atom = a
   end
-
+  
   def each(&blk)
     @left.each do |l|
       l.each(&blk)
@@ -29,13 +29,14 @@ end
 
 class TreeNode
   include Enumerable
-
+  
   attr_reader :minis
-
+  
   def initialize(m)
     @minis = m
   end
-
+  
+  
   # Invokes the block for each atom in the tree, doing an in-order traversal
   # according to the treedoc semantics.
   def each(&blk)
@@ -43,23 +44,104 @@ class TreeNode
       m.each(&blk)
     end
   end
-end
+  
+
+  # Finds treeNode at the end of specified path
+  def find_tree_node(path)
+    if path.first == nil
+      return self
+    elsif path.first == 1
+      return self.minis[minis.length - 1].right[0].find_tree_node(path[1..path.length])
+    elsif path.first == 0
+      return self.minis[0].left[0].find_tree_node(path[1..path.length])
+    end
+  end
+  
+  # checks if TreeNode's left child is nil
+  def check_empty_left()
+    if self.minis[0].left[0] == nil
+      return true
+    else
+      return false
+    end
+  end
+  
+  # checks if TreeNode's right child is nil
+  def check_empty_right()
+    if self.minis[minis.length - 1].right[0] == nil
+      return true
+    else
+      return false
+    end
+  end
+  
+  # Used when direct child of reference node is not nil.  Helps find spot in tree
+  # directly before reference node.
+  def find_farthest_right()
+    if self.minis[minis.length - 1].right[0] == nil
+      return self
+    else
+      return self.minis[minis.length -1].right[0].find_farthest_right()
+    end
+  end
+  
+  # Helps find spot in tree directly after reference node when right child is not nil
+  def find_farthest_left()
+    if self.minis[0].left[0] == nil
+      return self
+    else
+      return self.minis[0].left[0].find_farthest_left()
+    end
+  end
+  
+  # XXX: Insertion methods do not deal with disambiguators yet
+  
+  def insert_before(path, atom)
+    new_mini = MiniNode.new([],[], nil, atom)
+    new_node = TreeNode.new([new_mini])
+    referenceNode = self.find_tree_node(path)
+    if referenceNode.check_empty_left()
+      referenceNode.minis[0].left[0] = new_node
+    else
+      farthestRightNode = referenceNode.find_farthest_right()
+      farthestRightNode.minis[minis.length - 1].right[0] = new_node
+    end
+  end
+  
+  def insert_after(path, atom)
+    new_mini = MiniNode.new([],[], nil, atom)
+    new_node = TreeNode.new([new_mini])
+    referenceNode = self.find_tree_node(path)
+    if referenceNode.check_empty_right()
+      referenceNode.minis[minis.length - 1].right[0] = new_node
+    else
+      farthestLeftNode = referenceNode.find_farthest_left()
+      farthestLeftNode.minis[0].left[0] = new_node
+    end
+  end 
+  
+  
+end   
+
 
 class TreedocL < Bud::Lattice
   wrapper_name :tdoc
-
+  
   def initialize
     @v = []
   end
-
+  
   # XXX: monotone or morphism?
-  monotone :insert do |pos,atom|
+  monotone :insert_before do |pos,atom|   
   end
-
+  
+  monotone :insert_after do |pos,atom|
+  end
+  
   # XXX: monotone or morphism?
   monotone :delete do |pos|
   end
-
+  
   def contents
   end
 end
