4438231c85f0975296d63ab75f52d15d7d7fe907
Emily Andrews <emily@tesba.net>
2012-09-24 16:27:56 -0700

    Fixed lattice added

---
+++ Logoot/lfixed.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'bud'
+
+class FixedLattice < Bud::Lattice
+  wrapper_name :lfixed
+
+  def initialize(i=nil)
+  	@v = i
+  end
+
+  def merge(i)
+  	i_val = i.reveal
+    return self if i_val.nil?
+    return FixedLattice.new(i_val)
+  end
+end
+
+
---
+++ Logoot/tc_lfixed.rb
@@ -0,0 +1,42 @@
+require './test_common'
+require 'rubygems'
+require 'bud'
+require './lfixed'
+
+class SimpleLfixed
+	include Bud
+
+	state do 
+		lfixed :m1
+		lfixed :m2
+		lfixed :holder1
+		lfixed :holder2
+	end
+
+	bloom do
+		holder1 <= m1
+		holder2 <= m2
+	end
+end
+
+
+class TestLFixed < Test::Unit::TestCase
+	def test_basic
+		i = SimpleLfixed.new
+		line1 = "Concurrent editing is awesome"
+		line2 = "i <3 Logoot"
+		i.m1 <+ FixedLattice.new(line1)
+		i.m2 <+ FixedLattice.new(line2)
+		i.tick
+
+		assert_equal(line1, i.holder1.current_value.reveal)
+		assert_equal(line2, i.holder2.current_value.reveal)
+		i.tick
+		
+		assert_equal(line1, i.holder1.current_value.reveal)
+		assert_equal(line2, i.holder2.current_value.reveal)
+		i.m1 <+ FixedLattice.new("this should change")
+		i.tick
+		assert_equal("this should change", i.holder1.current_value.reveal)
+	end
+end
