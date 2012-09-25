4e582f29564a826c63044aea2bd4e23f78a9de7c
Emily Andrews <emily@tesba.net>
2012-09-24 20:15:36 -0700

    Initial logoot lattice stuff

---
+++ Logoot/logoot_lattice.rb
@@ -0,0 +1,62 @@
+require 'rubygems'
+require 'bud'
+require './lfixed'
+
+class Triple
+	include Comparable
+
+	attr_writer :num
+	attr_reader :num
+	attr_writer :site_id
+	attr_reader :site_id
+	attr_writer :time_stamp
+	attr_reader :time_stamp
+
+	def initialize(n, s, t)
+		@num = n
+		@site_id = s
+		@time_stamp = t
+	end
+
+	def <=> (other)
+		if @num > other.num
+			return 1
+		elsif other.num > @num
+			return -1
+		
+		elsif @site_id > other.site_id
+			return 1
+		elsif other.site_id > @site_id
+			return -1
+		
+		elsif @time_stamp > other.time_stamp
+			return 1
+		elsif other.time_stamp > @time_stamp
+			return -1
+		else
+			return 0
+		end		
+	end
+
+end
+
+class RecursiveLmap
+
+  attr_reader :line_id
+  attr_reader :text
+  
+  def initialize(line_id, text)
+    @line_id = line_id
+    @text = text
+  end
+
+  def create()
+    base = Hash[Triple.new(-1,-1,-1) => FixedLattice.new(@text)]
+    rlmap = Bud::MapLattice.new(base)
+    for t in line_id.reverse
+      rlmap = Bud::MapLattice.new(t => rlmap)
+    end
+    return rlmap
+  end
+
+end
---
+++ Logoot/tc_logoot_lattice.rb
@@ -0,0 +1,147 @@
+require './test_common'
+require 'rubygems'
+require 'bud'
+require './logoot_lattice'
+require "set"
+require './lfixed'
+
+class TripleTest < Test::Unit::TestCase
+
+	def testDiffNums
+		t1 = Triple.new(1, 1, 1)
+		t2 = Triple.new(2, 1, 1)
+
+		assert_equal(true, t1 < t2)
+		assert_equal(false, t1 > t2)
+	end
+
+	def testDiffSites
+		t3 = Triple.new(1, 1, 1)
+		t4 = Triple.new(1, 2, 1)
+
+		assert_equal(true, t4 > t3)
+	end
+
+	def testEquals
+		t1 = Triple.new(1,1,1)
+		t2 = Triple.new(1,1,1)
+
+		assert_equal(true, t1 == t1)
+	end
+
+	def testDiffTime
+		t1 = Triple.new(1, 1, 1)
+		t2 = Triple.new(1, 1, 2)
+
+		assert_equal(true, t2 > t1)
+	end
+
+	def testSortInSet
+		t1 = Triple.new(1,1,1)
+		t2 = Triple.new(2,2,2)
+		t3 = Triple.new(3,3,3)
+
+		mySet = SortedSet.new([t2, t3, t1])
+		myArr =  mySet.to_a()
+		
+		assert_equal(t1, myArr[0])
+		assert_equal(t2, myArr[1])
+		assert_equal(t3, myArr[2])
+
+	end
+end
+
+
+class TestRLmap < Test::Unit::TestCase
+	def testMakeRecursiveLmap
+		t1 = Triple.new(1,1,1)
+		t2 = Triple.new(2,2,2)
+		t3 = Triple.new(3,3,3)
+		line_id = [t1, t2, t3]
+		text = "hello world"
+		rlm = RecursiveLmap.new(line_id, text)
+		m = rlm.create()
+		assert_equal(m.key?(t1).reveal, true)
+		assert_equal(m.key?(t2).reveal, false)
+		secondLevel = m.at(t1)
+		assert_equal(secondLevel.key?(t2).reveal, true)
+		thirdLevel = secondLevel.at(t2)
+		assert_equal(thirdLevel.key?(t3).reveal, true)
+	end
+
+	def test_basic
+		t1 = Triple.new(1,1,1)
+		line_id = [t1]
+		text = "I love concurrent editing"
+		rlm = RecursiveLmap.new(line_id, text)
+		m = rlm.create()
+		assert_equal(m.key?(t1).reveal, true)
+	end
+end
+
+class SimpleLmap
+	include Bud
+
+	state do 
+		lmap :m1
+		lmap :m2
+		lmap :m3
+	end
+
+	bloom do
+		m1 <= m2
+		m1 <= m3
+	end
+end
+
+
+class TestLogootLattice <Test::Unit::TestCase
+	def test1
+		i = SimpleLmap.new
+		t1 = Triple.new(1,1,1)
+		t2 = Triple.new(2,2,2)
+		line_id1 = [t1]
+		line_id2 = [t2]
+		text1 = "foo"
+		text2 = "bar"
+
+		rlm1 = RecursiveLmap.new(line_id1, text1)
+		r1 = rlm1.create()
+		i.m3 <+ r1
+		rlm2 = RecursiveLmap.new(line_id2, text2)
+		r2 = rlm2.create()
+		i.m2 <+ r2
+		i.tick
+
+		assert_equal(i.m1.current_value.key?(t1).reveal, true)
+		assert_equal(i.m1.current_value.key?(t2).reveal, true)
+
+		t3 = Triple.new(3,4,5)
+		line_id3 = [t1, t3]
+		text3 = "baz"
+
+		rlm3 = RecursiveLmap.new(line_id3, text3)
+		i.m3 <+ rlm3.create()
+		i.tick
+
+		assert_equal(i.m1.current_value.key?(t1).reveal, true)
+		assert_equal(i.m1.current_value.key?(t2).reveal, true)
+		assert_equal(i.m1.current_value.key?(t3).reveal, false)
+
+		secondLevel = i.m1.current_value.at(t1)
+		assert_equal(secondLevel.key?(t3).reveal, true)
+	end
+
+	# More tests to come.
+end
+
+
+
+
+
+
+
+
+
+
+
