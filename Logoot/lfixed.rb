4fd1e95d9e9f631514d8d68e09bab8f5bdcb8eea
Emily Andrews <emily@tesba.net>
2012-10-01 14:54:57 -0700

    fixed indents

--- Logoot/lfixed.rb
+++ Logoot/lfixed.rb
@@ -5,18 +5,17 @@ class FixedLattice < Bud::Lattice
   wrapper_name :lfixed
 
   def initialize(i=nil)
-  	@v = i
+    @v = i
   end
 
   def merge(i)
-  	i_val = i.reveal
-  	return self if i_val.nil?
-  	if i_val != @v and @v != nil
-  		raise Bud::Error, "Cannot change fixed lattice. input = #{i.inspect}"
-  	end
+    i_val = i.reveal
+    return self if i_val.nil?
+    if i_val != @v and @v != nil
+      raise Bud::Error, "Cannot change fixed lattice. input = #{i.inspect}"
+    end
     return FixedLattice.new(i_val)
   end
-
 end
 
 
--- Logoot/tc_lfixed.rb
+++ Logoot/tc_lfixed.rb
@@ -4,43 +4,43 @@ require 'bud'
 require './lfixed'
 
 class SimpleLfixed
-	include Bud
+  include Bud
+  
+  state do 
+    lfixed :m1
+    lfixed :m2
+    lfixed :holder1
+    lfixed :holder2
+  end
 
-	state do 
-		lfixed :m1
-		lfixed :m2
-		lfixed :holder1
-		lfixed :holder2
-	end
-
-	bloom do
-		holder1 <= m1
-		holder2 <= m2
-	end
+  bloom do
+    holder1 <= m1
+    holder2 <= m2
+  end
 end
 
 
 class TestLFixed < Test::Unit::TestCase
-	def test_basic
-		i = SimpleLfixed.new
-		line1 = "Concurrent editing is awesome"
-		line2 = "i <3 Logoot"
-		i.m1 <+ FixedLattice.new(line1)
-		i.m2 <+ FixedLattice.new(line2)
-		i.tick
-
-		assert_equal(line1, i.holder1.current_value.reveal)
-		assert_equal(line2, i.holder2.current_value.reveal)
-		i.tick
-		
-		assert_equal(line1, i.holder1.current_value.reveal)
-		assert_equal(line2, i.holder2.current_value.reveal)
-
-		i.m1 <+ FixedLattice.new("this should not change")
-		
-		assert_raise(Bud::Error) do
-			i.tick
-		end
-
-	end
+  def test_basic
+    i = SimpleLfixed.new
+    line1 = "Concurrent editing is awesome"
+    line2 = "i <3 Logoot"
+    i.m1 <+ FixedLattice.new(line1)
+    i.m2 <+ FixedLattice.new(line2)
+    i.tick
+    
+    assert_equal(line1, i.holder1.current_value.reveal)
+    assert_equal(line2, i.holder2.current_value.reveal)
+    i.tick
+    
+    assert_equal(line1, i.holder1.current_value.reveal)
+    assert_equal(line2, i.holder2.current_value.reveal)
+    
+    i.m1 <+ FixedLattice.new("this should not change")
+    
+    assert_raise(Bud::Error) do
+      i.tick
+    end
+    
+  end
 end
