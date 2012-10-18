7e69ab3b180bcdf2577e361c5b3ea70bf4db2c4d
Emily Andrews <emily@tesba.net>
2012-10-17 23:24:09 -0700

    Insert-after and delete working

--- Logoot/gui.rb
+++ Logoot/gui.rb
@@ -5,15 +5,6 @@ require 'rubygems'
 require './lfixed'
 require 'pp'
 
-class CustomCellRend < Gtk::CellRendererText
-  def initialize
-    super
-    self.text = ""
-  end
-
-end
-
-
 class LatticeDocGUI
 
   attr_accessor :lmap
@@ -29,62 +20,39 @@ class LatticeDocGUI
 
     listStore = Gtk::ListStore.new(Array, String, String)
     treeView1 = Gtk::TreeView.new(listStore)
-    if lmap != nil
-      #loadDocument(lmap, listStore, [])
-    else 
-      parent = listStore.append
-      parent.set_value(0, [-2,-2,-2])
-      parent.set_value(1, "")
-    end
     firstRow = listStore.append
     treeView1.selection.mode = Gtk::SELECTION_SINGLE
     renderer = Gtk::CellRendererText.new
-    customRenderer = CustomCellRend.new
-    col2 = Gtk::TreeViewColumn.new("Position ID", customRenderer, :text => 0)
-    #treeView1.append_column(col2)
-
-
+    col2 = Gtk::TreeViewColumn.new("Position ID", renderer, :text => 0)
     col = Gtk::TreeViewColumn.new("Document", renderer, :text => 1)
     treeView1.append_column(col)
-    
     col3 = Gtk::TreeViewColumn.new("Position ID- String", renderer, :text => 2)
     treeView1.append_column(col3)
 
-    #beforeButton = Gtk::Button.new("Insert Before")
+
     afterButton = Gtk::Button.new("Insert After")
+    deleteButton = Gtk::Button.new("Delete")
     entry = Gtk::Entry.new
 
     vbox = Gtk::VBox.new(homogeneous=false, spacing=nil) 
     vbox.pack_start_defaults(treeView1)
-    #vbox.pack_start_defaults(beforeButton)
+    vbox.pack_start_defaults(deleteButton)
     vbox.pack_start_defaults(afterButton)
     vbox.pack_start_defaults(entry)
 
     iter = nil
-    #iterPrev = nil
-
 
     treeView1.signal_connect("row-activated") do |view, path, column|
       iter = treeView1.model.get_iter(path)
       p "Current line id of selection"
       pp listStore.get_value(iter, 0)
-      #iterPrev = treeView1.model.get_iter(path.prev!)
     end
-
-    #beforeButton.signal_connect( "clicked" ) do |w|
-    #  newRow = listStore.insert_before(iter)
-    #  oldID = listStore.get_value(iterPrev, 0)
-    #  newID = genPosIdAfter(oldID, 5)
-    #  puts newID
-    #  listStore.set_value(newRow, 0, newID)
-    #  listStore.set_value(newRow, 1, entry.text)
-    #end
-
+   
     afterButton.signal_connect( "clicked" ) do |w|
 
       newRow = listStore.insert_after(iter)
       oldID = listStore.get_value(iter, 0)
-      if oldID == false
+      if oldID == false or oldID == nil
         temp = false
       else
         temp = oldID.clone
@@ -96,11 +64,17 @@ class LatticeDocGUI
       myText = entry.text
       listStore.set_value(newRow, 1, myText)
       rlm = RecursiveLmap.new(newID, myText).create()
-      p rlm
-      @lmap.merge(rlm)
-
+      @lmap = @lmap.merge(rlm)
+      p @lmap
+      prp.printDocument(@lmap)
     end
 
+    deleteButton.signal_connect("clicked") do |w|
+      id = listStore.get_value(iter,0)
+      rlm = RecursiveLmap.new(id, -1 ).create()
+      @lmap = @lmap.merge(rlm)
+      treeView1.model.remove(iter)
+    end
 
     window = Gtk::Window.new("LatticeDoc")
     window.signal_connect("destroy") { Gtk.main_quit }
@@ -108,31 +82,6 @@ class LatticeDocGUI
     window.show_all
     Gtk.main
   end
-  
-
-
-  def loadDocument(lmap, treestore, posID)
-    print "HERE"
-    sortedKeys = lmap.reveal.keys.sort
-    for key in sortedKeys
-      #posID.concat([key]) unless key == [-1,-1,-1]
-      if key == [-1,-1,-1]
-        if lmap.reveal.values_at(key)[0].reveal != -1
-          parent = treestore.append
-          parent.set_value(0, [[-3,-3,-3]])
-          parent.set_value(1, lmap.reveal.values_at(key)[0].reveal)
-        end
-        next
-      end
-      nextLmap = lmap.reveal[key]
-      loadDocument(nextLmap, treestore, posID)
-    end
-  end
-
-
 end
 
-#rlm = RecursiveLmap.new([[-1,-1,-1]], "First line")
-#lmap = rlm.create()
-#yGui = LatticeDocGUI.new(lmap, 1)
 
