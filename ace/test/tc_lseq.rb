4134ac60e29a7e4939802f72f94a060559a5f286
Neil Conway <nrc@cs.berkeley.edu>
2013-02-01 22:58:30 -0800

    More lseq tests.

--- ace/test/tc_lseq.rb
+++ ace/test/tc_lseq.rb
@@ -10,18 +10,33 @@ class SeqTest < MiniTest::Unit::TestCase
     assert_raises(Bud::TypeError) { SeqLattice.new([true]) }
   end
 
+  def check_permutations(ops)
+    ops.permutation.each do |x|
+      ops.permutation.each do |y|
+#        v = x.reduce(:merge)
+#        puts "v = #{v.inspect}"
+        assert(x.reduce(:merge) == y.reduce(:merge))
+      end
+    end
+  end
+
   def test_singleton
     x = SeqLattice.new([5])
     y = SeqLattice.new([10])
-    assert_equal(x, x.merge(x))
-    assert_equal(y, y.merge(y))
+
+    check_permutations([x, y])
     m1 = x.merge(y)
     m2 = y.merge(x)
-    assert_equal(SeqLattice.new([5, 10]), m1)
-    assert_equal(SeqLattice.new([5, 10]), m2)
     assert_equal(Bud::MaxLattice.new(2), m1.size)
     assert_equal(Bud::MaxLattice.new(2), m2.size)
     assert_equal(Bud::SetLattice.new([10, 5]), m1.elements)
     assert_equal(Bud::SetLattice.new([10, 5]), m2.elements)
+  end
+
+  def test_basic
+    ops = (1..4).map {|i| SeqLattice.new([i])}
+    ops.combination(2).each {|o| check_permutations(o)}
+    ops.combination(3).each {|o| check_permutations(o)}
+    check_permutations(ops)
   end
 end
