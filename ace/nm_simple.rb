d7b61558bd559062fb71018b4bcc192ea2ba42a5
Neil Conway <nrc@cs.berkeley.edu>
2013-03-29 10:52:27 -0700

    Change representation of begin/end sentinels.
    Previously, the sentinels involved a cylic dependency:
    
        BEGIN.post = END, END.pre = BEGIN
    
    That isn't a crazy thing to do, but introducing a cycle into the dependency
    graph causes various complications. It seems better to have only a single
    constraint (e.g., BEGIN.post = END). In a subsequent commit, we'll require that
    no constraints have END as their pre link; hence, END should still appear at the
    end of the document.

--- ace/nm_simple.rb
+++ ace/nm_simple.rb
@@ -1,8 +1,15 @@
 require 'rubygems'
 require 'bud'
 
-BEGIN_ID = "__begin__"
-END_ID = "__end__"
+# Float::INFINITY only defined in MRI 1.9.2+
+unless defined? Float::INFINITY
+  Float::INFINITY = 1.0/0.0
+end
+
+# Sentinel edit IDs. Note that the tiebreaker for sentinels should never be
+# used, so to test this we have BEGIN_ID > END_ID.
+BEGIN_ID = Float::INFINITY
+END_ID = -Float::INFINITY
 
 # TODO: add-back integrity constraints
 class SimpleNmLinear
@@ -12,8 +19,9 @@ class SimpleNmLinear
     # Input: the constraint that the given ID must follow the "pre" node and
     # precede the "post" node. This encodes a DAG.
     table :constr, [:id] => [:pre, :post]
-    scratch :u_constr, constr.schema    # Non-sentinal constraints
     scratch :constr_prod, [:x, :y]      # Product of constr with itself
+    scratch :pre_constr, constr.schema  # Constraints with valid "pre" edge
+    scratch :post_constr, constr.schema # Constraints with valid "post" edge
 
     # Output: the computed linearization of the DAG
     scratch :before, [:from, :to]
@@ -38,23 +46,26 @@ class SimpleNmLinear
   end
 
   bootstrap do
+    # Sentinel constraints. We choose to have END be the causally first edit;
+    # then BEGIN is placed before the END. Naturally these could be reversed.
     constr <+ [[BEGIN_ID, nil, END_ID],
-               [END_ID, BEGIN_ID, nil]]
+               [END_ID, nil, nil]]
   end
 
   bloom :constraints do
-    u_constr <= constr {|c| c unless [BEGIN_ID, END_ID].include? c.id}
+    pre_constr <= constr {|c| c unless [BEGIN_ID, END_ID].include? c.id}
+    post_constr <= constr {|c| c unless c.id == END_ID}
     constr_prod <= (constr * constr).pairs {|c1,c2| [c1.id, c2.id]}
   end
 
   bloom :compute_sem_hist do
-    sem_hist <= constr {|c| [c.id, c.pre] unless c.id == BEGIN_ID}
-    sem_hist <= constr {|c| [c.id, c.post] unless c.id == END_ID}
-    sem_hist <= (sem_hist * constr).pairs(:to => :id) do |r,c|
-      [r.from, c.pre] unless c.id == BEGIN_ID
+    sem_hist <= pre_constr {|c| [c.id, c.pre]}
+    sem_hist <= post_constr {|c| [c.id, c.post]}
+    sem_hist <= (sem_hist * pre_constr).pairs(:to => :id) do |r,c|
+      [r.from, c.pre]
     end
-    sem_hist <= (sem_hist * constr).pairs(:to => :id) do |r,c|
-      [r.from, c.post] unless c.id == END_ID
+    sem_hist <= (sem_hist * post_constr).pairs(:to => :id) do |r,c|
+      [r.from, c.post]
     end
     sem_hist_prod <= (sem_hist * sem_hist).pairs do |h1,h2|
       [h1.from, h1.to, h2.from, h2.to]
@@ -63,8 +74,8 @@ class SimpleNmLinear
 
   # Compute each of explicit, implied_parent, and tie_break.
   bloom :compute_candidates do
-    explicit <= constr {|c| [c.pre, c.id] unless c.id == BEGIN_ID}
-    explicit <= constr {|c| [c.id, c.post] unless c.id == END_ID}
+    explicit <= pre_constr {|c| [c.pre, c.id]}
+    explicit <= post_constr {|c| [c.id, c.post]}
 
     tie_break <= constr_prod {|p| [p.x, p.y] if p.x < p.y}
 
--- ace/test/tc_nm_simple.rb
+++ ace/test/tc_nm_simple.rb
@@ -2,12 +2,31 @@ require_relative 'test_common'
 require_relative '../nm_simple'
 
 class NmSimpleTest < MiniTest::Unit::TestCase
-  def test_sem_hist
+  def test_empty_doc
     s = SimpleNmLinear.new
     s.tick
-    assert_equal([[BEGIN_ID, BEGIN_ID],
-                  [END_ID, BEGIN_ID],
-                  [END_ID, END_ID],
-                  [BEGIN_ID, END_ID]].sort, s.sem_hist.to_a.sort)
+    assert_equal([[BEGIN_ID, END_ID]], s.sem_hist.to_a)
+    assert_equal([[BEGIN_ID, END_ID]], s.before.to_a)
+    assert_equal([[BEGIN_ID, END_ID]], s.before_tc.to_a)
+    assert_equal([[BEGIN_ID, END_ID]], s.explicit.to_a)
+    assert_equal([], s.implied_parent.to_a)
+  end
+
+  def test_explicit_simple
+    s = SimpleNmLinear.new
+    s.tick
+  end
+
+  def test_tiebreak_simple
+  end
+
+  # 3 -> 1 is clear, but 2 might plausibly be placed anywhere with respect to 3
+  # and 1: [2,3,1], [3,2,1], [3,1,2] all respect the explicit constraints.
+  # However, since we would order 1 -> 2 given only those two atoms, this
+  # implied constraint must also be respected. Hence, [3,1,2] is the only
+  # correct outcome. Note that this scenario is essentially identical to the
+  # "dOPT Puzzle" described in "Operational transformation in real-time group
+  # editors: issues, algorithms, and achievements" (Sun and Ellis, CSCW'98).
+  def test_implied_parent_simple
   end
 end
