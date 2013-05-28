814a9f30b51298380acb66de4ccaab881fcf1366
Neil Conway <nrc@cs.berkeley.edu>
2013-05-27 22:04:02 -0700

    Checkpoint.

--- dice/nm_tree.rb
+++ dice/nm_tree.rb
@@ -9,14 +9,56 @@ end
 BEGIN_NODE = -Float::INFINITY
 END_NODE = Float::INFINITY
 
-# Goal: use the tree-like stuff described in tree.rb to implement a concurrent
+# Goal: use something akin to SimpleTree (tree.rb) to implement a concurrent
 # editing scheme.
 #
 # Differences:
-#   (a) in tree.rb, the order between atoms is given by a scalar comparison of
-#       their IDs. In concurrent editing, the _explicit_ constraints are given
-#       by the contents of a table.
-#   (b) in tree.rb, atom ID is a total order. In concurrent editing, the
+#   (a) in SimpleTree, the order between atoms is given by a scalar comparison
+#       of their IDs. In concurrent editing, the _explicit_ constraints are
+#       given by the contents of a table.
+#
+#   (b) Additional details: the explicit constraints include both "pre" and
+#       "post" nodes. Also, we want to consider the transitive consequences of
+#       the explicit constraints, ideally without enumerating all of them.
+#
+#   (c) in SimpleTree, atom ID is a total order. In concurrent editing, the
 #       explicit constraints only form a partial order. We'd then like to
 #       resolve ambiguous situations by considering tiebreak and implied-by-anc
 #       orders.
+#
+#   (d) another difference is that in SimpleTree, we start at the root when
+#       trying to determine where a new insertion appears. In concurrent
+#       editing, we have PRE and POST constraints; we know that the edit must
+#       appear between these two nodes (according to the tree traversal order),
+#       so we can immediately jump to either PRE or POST node and then walk
+#       forward/backward from there.
+#
+# If we represent a partial order using SimpleTree, a node in the tree might
+# have multiple left or right children. Naturally, we then want to assign an
+# ordering to these children (i.e., find a monotone linearization of the input
+# constraint set). One question is whether, given the explicit constraints and
+# some sort of tiebreaking scheme, we should include the "tiebreaking" implied
+# constraints in the tree, or whether those should be represented differently.
+class TreeLinear
+  include Bud
+
+  state do
+    table :root, [] => [:id]
+    table :edge, [:from, :to, :kind]
+
+    table :constr, [:id] => [:pre, :post]
+
+    # Insert operation state (transient)
+    scratch :ins_init, [:op_id]
+    scratch :ins_state, [:op_id, :node_id, :depth]
+  end
+
+  bloom do
+    ins_state <= (ins_init * root).pairs {|i,r| [i.op_id, r.id, 0]}
+
+    # Walk the tree to find the first position that satisfies the "pre"
+    # constraint. That is, we check whether 
+    ins_state <= (ins_state * edge).pairs(:node_id => :from) do |i,e|
+    end
+  end
+end
--- dice/tree.rb
+++ dice/tree.rb
@@ -26,7 +26,7 @@ class SimpleTree
     # to allow multiple operations to be in-flight at once, but this doesn't
     # currently work.
     scratch :ins_init, [:op_id]
-    scratch :ins_curr, [:op_id, :node_id, :depth]
+    scratch :ins_state, [:op_id, :node_id, :depth]
 
     scratch :max_depth, [:op_id] => [:depth]
   end
@@ -37,12 +37,12 @@ class SimpleTree
   end
 
   bloom do
-    ins_curr <= (ins_init * root).pairs {|i,r| [i.op_id, r.node_id, 0]}
+    ins_state <= (ins_init * root).pairs {|i,r| [i.op_id, r.node_id, 0]}
 
     # If the to-be-inserted element is smaller than the current node, it must go
     # to the left. If it is larger, it must go to the right.
     # XXX: for now, we just assume that the nodes are ordered by ID
-    ins_curr <= (ins_curr * edge).pairs(:node_id => :from) do |i,e|
+    ins_state <= (ins_state * edge).pairs(:node_id => :from) do |i,e|
       if e.kind == :left
         [i.op_id, e.to, i.depth + 1] if i.op_id < e.from
       elsif e.kind == :right
@@ -54,8 +54,8 @@ class SimpleTree
     # we add a new edge corresponding to the insertion.
     # NB: Alternate implementation. We could use an ldom, where the version is
     # an lmax (depth) and the value is the node ID at that depth.
-    max_depth <= ins_curr.group([:op_id], max(:depth))
-    edge <+ (max_depth * ins_curr).rights(:op_id => :op_id, :depth => :depth) do |i|
+    max_depth <= ins_state.group([:op_id], max(:depth))
+    edge <+ (max_depth * ins_state).rights(:op_id => :op_id, :depth => :depth) do |i|
       [i.node_id, i.op_id, i.op_id < i.node_id ? :left : :right]
     end
   end
